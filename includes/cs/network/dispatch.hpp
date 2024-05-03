37374912f4cf988d0af8a758cb3d2b69f4e5f27f
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-05-03 02:23:41 +0200

    remove template and dispatch is now lazy shared class

--- includes/cs/network/dispatch.hpp
+++ includes/cs/network/dispatch.hpp
@@ -5,18 +5,10 @@
 #if ___cs_requirements
 
 #include "cs/os.hpp"
-#include "cs/diagnostics/exception.hpp"
 #include "cs/system/descriptor.hpp"
 #include "cs/containers/vector.hpp"
-
-#if defined(___cs_os_macos)
-#	include <sys/event.h>
-#	include <sys/time.h>
-#elif defined(___cs_os_linux)
-#	include <sys/epoll.h>
-#endif
-
-#include <iostream>
+#include "cs/io_event.hpp"
+#include "cs/network/event.hpp"
 
 
 // -- C S  N A M E S P A C E --------------------------------------------------
@@ -24,45 +16,17 @@
 namespace cs {
 
 
-	// -- non-member constants ------------------------------------------------
-
-	#if defined(___cs_os_macos)
-
-	/* event flag type */
-	using ev_flag = int;
-
-	/* dispatch event */
-	enum dispatch_event : cs::ev_flag {
-		EV_READ  = EVFILT_READ,
-		EV_WRITE = EVFILT_WRITE,
-		EV_ERROR = EVFILT_ERROR
-	};
-
-	#elif defined(___cs_os_linux)
-
-	/* event flag type */
-	using ev_flag = ::uint32_t;
-
-	/* dispatch event */
-	enum dispatch_event : cs::ev_flag {
-		EV_READ  = EPOLLIN,
-		EV_WRITE = EPOLLOUT,
-		EV_ERROR = EPOLLERR
-	};
-	#endif
-
-
 	// -- D I S P A T C H -----------------------------------------------------
 
-	template <typename ___type>
 	class dispatch {
+
 
 		private:
 
 			// -- private types -----------------------------------------------
 
 			/* self type */
-			using ___self = cs::dispatch<___type>;
+			using ___self = cs::dispatch;
 
 			/* event type */
 			#if defined(___cs_os_macos)
@@ -104,36 +68,6 @@ namespace cs {
 						| ~EVFILT_SYSCOUNT
 			};
 
-			// -1  1111111111111111
-			// -2  1111111111111110
-			// -3  1111111111111101
-			// -4  1111111111111100
-			// -5  1111111111111011
-			// -6  1111111111111010
-			// -7  1111111111111001
-			// -8  1111111111111000
-			// -9  1111111111110111
-			// -10 1111111111110110
-			// -12 1111111111110100
-			// -15 1111111111110001
-			// -18 1111111111101110
-
-			// inverted
-			// 0   0000000000000000
-			// 1   0000000000000001
-			// 2   0000000000000010
-			// 3   0000000000000011
-			// 4   0000000000000100
-			// 5   0000000000000101
-			// 6   0000000000000110
-			// 7   0000000000000111
-			// 8   0000000000001000
-			// 9   0000000000001001
-			// 10  0000000000001010
-			// 12  0000000000001100
-			// 15  0000000000001111
-			// 18  0000000000010010
-
 
 			template <cs::ev_flag ___ev>
 			static constexpr bool is_event = (~___ev & EV_MASK) == 0;
@@ -157,12 +91,6 @@ namespace cs {
 
 		private:
 
-			// -- public types ------------------------------------------------
-
-			/* observer type */
-			using observer = ___type;
-
-
 			// -- private members ---------------------------------------------
 
 			/* handle */
@@ -174,32 +102,40 @@ namespace cs {
 
 		public:
 
-			// -- public lifecycle --------------------------------------------
+			// -- public methods ----------------------------------------------
 
-			/* default constructor */
-			dispatch(void)
-			#if defined(___cs_os_macos)
-			: _handle{::kqueue()},
-			#elif defined(___cs_os_linux)
-			: _handle{::epoll_create1(0)},
-			#endif
-			_events{}
-				{
+			/* poll */
+			static auto poll(void) -> void;
 
-				// check for error
-				if (not _handle)
-					throw cs::runtime_error{"failed to create kqueue"};
+			/* add */
+			template <cs::ev_flag ___evs>
+			static auto add(cs::io_event& ___obs) -> void {
 
-				// allocate events
-				_events.resize(64);
+				// check for valid event
+				static_assert(___self::is_event<___evs>, "dispatch: event not supported");
+
+				// call add
+				___self::_shared()._add(___evs, ___obs);
 			}
 
+			/* remove */
+			static auto remove(cs::io_event&) -> void;
+
+
+
+
+		private:
+
+			// -- private lifecycle -------------------------------------------
+
+			/* default constructor */
+			dispatch(void);
 
 			/* deleted copy constructor */
 			dispatch(const ___self&) = delete;
 
-			/* move constructor */
-			dispatch(___self&&) noexcept = default;
+			/* deleted move constructor */
+			dispatch(___self&&) = delete;
 
 			/* destructor */
 			~dispatch(void) noexcept = default;
@@ -210,176 +146,36 @@ namespace cs {
 			/* deleted copy assignment operator */
 			auto operator=(const ___self&) -> ___self& = delete;
 
-			/* move assignment operator */
-			auto operator=(___self&&) noexcept -> ___self& = default;
+			/* deleted move assignment operator */
+			auto operator=(___self&&) -> ___self& = delete;
 
 
-			// -- public methods ----------------------------------------------
+			// -- private static methods --------------------------------------
 
-			/* wait */
-			auto wait(void) -> void {
-
-				// wait for events
-				const auto nev = _wait();
-
-				// process events
-				for (___size i = 0U; i < nev; ++i) {
-					// get event flag
-					const cs::ev_flag ev = _event(_events[i]);
-					// get observer
-					observer& ob = _data(_events[i]);
-					// dispatch event
-					ob.dispatch(ev);
-				}
-
-				// resize events
-				if (nev == _events.size())
-					_events.resize(_events.size() * 2U);
-			}
+			/* shared */
+			static auto _shared(void) -> ___self&;
 
 
-			// -- public modifiers --------------------------------------------
+			/* data */
+			static auto _data(___event&) noexcept -> cs::io_event&;
 
-			/* add */
-			template <cs::ev_flag ___evs>
-			static auto add(const ___self& ___disp, observer& ___obs) -> void {
+			/* event */
+			static auto _event(___event&) noexcept -> cs::ev_flag;
 
-				// check for valid event
-				static_assert(___self::is_event<___evs>, "dispatch: event not supported");
-
-				#if defined(___cs_os_macos)
-
-				// create event
-				struct kevent event {
-					.ident  = static_cast<uintptr_t>(___obs.descriptor()),
-					.filter = ___evs,
-					.flags  = EV_ADD,
-					.fflags = 0U,
-					.data   = 0,
-					.udata  = &___obs
-				};
-
-				// EV_CLEAR info: make
-
-				// add event
-				if (::kevent(___disp._handle, &event, 1, nullptr, 0, nullptr) != 0)
-					throw cs::runtime_error{"failed to add descriptor to kqueue"};
-
-				#elif defined(___cs_os_linux)
-
-				// create event
-				struct epoll_event event {
-					.events = ___evs,
-					.data = {
-						.ptr = &___obs }
-				};
-
-				// add event
-				if (::epoll_ctl(___disp._handle, EPOLL_CTL_ADD, ___obs.descriptor(), &event) == -1)
-					throw cs::runtime_error{"failed to add descriptor to epoll"};
-
-				#endif
-			}
-
-
-			/* remove */
-			static auto remove(const ___self& ___disp, observer& ___obs) -> void {
-
-				#if defined(___cs_os_macos)
-
-				// create event
-				struct kevent event {
-					.ident  = static_cast<uintptr_t>(___obs.descriptor()),
-					.filter = EVFILT_READ,
-					.flags  = EV_DELETE,
-					.fflags = 0U,
-					.data   = 0,
-					.udata  = &___obs
-				};
-
-				std::cout << "removing descriptor from kqueue: " << ___obs.descriptor() << std::endl;
-
-				// remove event
-				if (::kevent(___disp._handle, &event, 1, nullptr, 0, nullptr) == -1) {
-					perror("kevent");
-					throw cs::runtime_error{"failed to remove descriptor from kqueue"};
-				}
-
-				#elif defined(___cs_os_linux)
-
-				// create event
-				struct epoll_event event {
-					.events = 0,
-					.data   = {reinterpret_cast<void*>(___obs.descriptor())}
-				};
-
-				// remove event
-				if (::epoll_ctl(___disp._handle, EPOLL_CTL_DEL, ___obs.descriptor(), &event) == -1)
-					throw cs::runtime_error{"failed to remove descriptor from epoll"};
-
-				#endif
-			}
-
-
-		private:
 
 			// -- private methods ---------------------------------------------
 
-			/* data */
-			static auto _data(___event& ___ev) -> observer& {
-				#if defined(___cs_os_macos)
-				return *reinterpret_cast<observer*>(___ev.udata);
-				#elif defined(___cs_os_linux)
-				return *reinterpret_cast<observer*>(___ev.data.ptr);
-				#endif
-			}
-
-			/* event */
-			static auto _event(___event& ___ev) -> cs::ev_flag {
-				#if defined(___cs_os_macos)
-				return ___ev.filter;
-				#elif defined(___cs_os_linux)
-				return ___ev.events;
-				#endif
-			}
-
+			/* poll */
+			auto _poll(void) -> void;
 
 			/* wait */
-			auto _wait(void) -> ___size {
+			auto _wait(void) -> ___size;
 
-				// -- macos ---------------------------------------------------
+			/* add */
+			auto _add(const cs::ev_flag, cs::io_event&) -> void;
 
-				#if defined(___cs_os_macos)
-				// compute timeout
-				constexpr struct ::timespec timeout {
-					.tv_sec  =  DISPATCH_TIMEOUT / 1'000U,
-					.tv_nsec = (DISPATCH_TIMEOUT % 1'000U) * 1'000'000U
-				};
-				// wait for events
-				const auto ret = ::kevent(_handle, nullptr, 0, _events.data(),
-																static_cast<int>(_events.size()),
-																&timeout);
-
-
-				// -- linux ---------------------------------------------------
-
-				#elif defined(___cs_os_linux)
-				// wait for events
-				const auto ret = ::epoll_wait(_handle, _events.data(),
-														static_cast<int>(_events.size()),
-														DISPATCH_TIMEOUT);
-				// see epoll_pwait
-				#endif
-
-
-				// -- common --------------------------------------------------
-
-				// check for error
-				if (ret < 0)
-					throw cs::runtime_error{"failed to wait for events"};
-
-				return static_cast<___size>(ret);
-			}
+			/* remove */
+			auto _remove(cs::io_event&) -> void;
 
 	}; // class dispatch
 
