58fc7f98a87bee4d431065cf8a539f08f21fa678
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-05-03 05:45:19 +0200

    add input.hpp

---
+++ includes/cs/system/input.hpp
@@ -0,0 +1,79 @@
+#ifndef ___CS_SYSTEM_INPUT_HPP___
+#define ___CS_SYSTEM_INPUT_HPP___
+
+#include "cs/config.hpp"
+#if ___cs_requirements
+
+#include "cs/io_event.hpp"
+#include <termios.h>
+
+
+// -- C S  N A M E S P A C E --------------------------------------------------
+
+namespace cs {
+
+
+	// -- I N P U T -----------------------------------------------------------
+
+	class input final : public cs::io_event {
+
+
+		private:
+
+			// -- private types -----------------------------------------------
+
+			/* self type */
+			using ___self = cs::input;
+
+
+			// -- private members ---------------------------------------------
+
+			/* terminal attributes */
+			struct termios _term;
+
+
+		public:
+
+
+			// -- public lifecycle --------------------------------------------
+
+			/* default constructor */
+			input(void);
+
+			/* deleted copy constructor */
+			input(const ___self&) = delete;
+
+			/* move constructor */
+			input(___self&&) noexcept = default;
+
+			/* destructor */
+			virtual ~input(void) noexcept;
+
+
+			// -- public assignment operators ---------------------------------
+
+			/* deleted copy assignment operator */
+			auto operator=(const ___self&) -> ___self& = delete;
+
+			/* move assignment operator */
+			auto operator=(___self&&) noexcept -> ___self& = default;
+
+
+			// -- public overriden methods ------------------------------------
+
+			/* read */
+			auto read(void) -> void override;
+
+			/* send */
+			auto send(void) -> void override;
+
+			/* descriptor */
+			auto descriptor(void) const noexcept -> int override;
+
+
+	}; // class input
+
+} // namespace cs
+
+#endif // ___cs_requirements
+#endif // ___CS_SYSTEM_INPUT_HPP___
