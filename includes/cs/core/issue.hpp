89bb692e0c679b35f18e43d2e2d202d18e55c91d
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-05-07 04:23:44 +0200

    implement methods

--- includes/cs/core/issue.hpp
+++ includes/cs/core/issue.hpp
@@ -3,6 +3,7 @@
 
 #include "cs/config.hpp"
 #include "cs/containers/vector.hpp"
+#include "cs/string.hpp"
 
 
 #if ___cs_requirements
@@ -11,6 +12,117 @@
 // -- C S  N A M E S P A C E --------------------------------------------------
 
 namespace cs {
+
+
+	// -- C O R E -------------------------------------------------------------
+
+	namespace core {
+
+
+		// -- I S S U E -------------------------------------------------------
+
+		class issue final {
+
+
+			public:
+
+				// -- public types --------------------------------------------
+
+				/* size type */
+				using size_type = cs::size_t;
+
+
+			private:
+
+				// -- private types -------------------------------------------
+
+				/* self type */
+				using ___self = cs::core::issue;
+
+
+				// -- private members -----------------------------------------
+
+				/* line */
+				size_type _line;
+
+				/* column */
+				size_type _column;
+
+				/* message */
+				cs::string _message;
+
+				/* snippet */
+				cs::string _snippet;
+
+
+			public:
+
+				// -- public lifecycle ----------------------------------------
+
+				/* default constructor */
+				issue(void) noexcept = default;
+
+				/* deleted copy constructor */
+				issue(const ___self&) = delete;
+
+				/* move constructor */
+				issue(___self&&) noexcept = default;
+
+				/* destructor */
+				~issue(void) noexcept = default;
+
+
+				// -- public assignment operators -----------------------------
+
+				/* deleted copy assignment operator */
+				auto operator=(const ___self&) -> ___self& = delete;
+
+				/* move assignment operator */
+				auto operator=(___self&&) -> ___self& = default;
+
+
+				// -- public accessors ----------------------------------------
+
+				/* line */
+				auto line(void) noexcept -> cs::size_t&;
+
+				/* const line */
+				auto line(void) const noexcept -> const cs::size_t&;
+
+				/* column */
+				auto column(void) noexcept -> cs::size_t&;
+
+				/* const column */
+				auto column(void) const noexcept -> const cs::size_t&;
+
+				/* message */
+				auto message(void) noexcept -> cs::string&;
+
+				/* const message */
+				auto message(void) const noexcept -> const cs::string&;
+
+				/* snippet */
+				auto snippet(void) noexcept -> cs::string&;
+
+				/* const snippet */
+				auto snippet(void) const noexcept -> const cs::string&;
+
+
+				// -- public modifiers ----------------------------------------
+
+				/* clear */
+				auto clear(void) noexcept -> void;
+
+
+				// -- public methods ------------------------------------------
+
+				/* print */
+				auto print(void) const noexcept -> void;
+
+
+		}; // class issue
+
+	} // namespace core
 
 
 	// -- I S S U E -----------------------------------------------------------
@@ -141,5 +253,4 @@ namespace cs {
 } // namespace cs
 
 #endif // ___cs_requirements
-
 #endif // CS_ISSUE_HEADER
